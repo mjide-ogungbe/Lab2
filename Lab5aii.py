0be209049a40d63a6d5caffc411bb6e777b8ae77
mjide-ogungbe <156146122+mjide-ogungbe@users.noreply.github.com>
2024-02-12 15:26:52 -0500

    Add files via upload

---
+++ Lab5aii.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys 
+if len(sys.argv) !=3:
+    print(f'Usage: {sys.argv[0]} <name> <age> ')
+    sys.exit(1)
+
+else:
+    print("Input three arguments in the command line")
+
+
+# Assign the first command-line arguement to the string object called "name"
+name = sys.argv[1]
+
+# Assign the Second command-line arguement to the integar object called "age
+# "
+age = sys.argv[2]
+
+# A print statement that says the below
+
+print('Hi ' + (str(sys.argv[1])) + ','  ' you are ' +(str(int(sys.argv[2]))) + ' years old' 
+)
